72297a75c406cb55263f5d8c15fc5402c1e6aefd
Fratu <143589461+syarasyoujyu@users.noreply.github.com>
2024-11-20 12:50:19 +0900

    app.py修正

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import torch
 from backend.const import JANKEN_LABELS, MODEL_PATH
 from backend.hand_tracker import HandTracker
 from backend.model import LSTMNet
+from streamlit.delta_generator import DeltaGenerator
 
 # モデルの初期設定
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -19,11 +20,12 @@ st.title("リアルタイムじゃんけんゲーム")
 
 stop_event = asyncio.Event()
 start_event = asyncio.Event()
+start_event.clear()
 # Mediapipe の Hands モジュールの初期化
 hand_tracker = HandTracker()
 
 
-async def janken_game() -> None:
+async def janken_game(frame_placeholder: DeltaGenerator) -> None:
     hc = None
     cap = cv2.VideoCapture(0)
     while cap.isOpened():
@@ -58,19 +60,18 @@ async def janken_game() -> None:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         # 手のランドマークが検出された場合、描画する
-        hand_tracker.draw_landmarks(image, hand_landmarks)
+        hand_tracker.draw_landmarks(image, multi_hand_landmarks)
 
         # 結果の画像を表示
         frame_placeholder.image(image, channels="BGR")
 
-        if stop_event.is_set():
-            if not st.session_state["set_finish_button"]:
-                st.session_state["set_finish_button"] = True
-                if st.button("ゲーム終了"):
-                    start_event.clear()
-                    frame_placeholder.empty()
-                    st.write("ゲームを終了します。")
-                    break
+        if stop_event.is_set() and not st.session_state["set_finish_button"]:
+            st.session_state["set_finish_button"] = True
+            if st.button("ゲーム終了"):
+                stop_event.clear()
+                st.session_state["wrote_janken_result"] = False
+                st.session_state["set_finish_button"] = False
+                break
         # whileを円滑に処理するためのawait
         await asyncio.sleep(0.01)
 
@@ -96,17 +97,31 @@ async def timer() -> None:
     stop_event.set()
 
 
-async def main() -> None:
-    janken = asyncio.create_task(janken_game())
+async def main_game(frame_placeholder: DeltaGenerator) -> None:
+    janken = asyncio.create_task(janken_game(frame_placeholder))
     time = asyncio.create_task(timer())
     st.session_state.wrote_janken_result = False
+    st.session_state.set_finish_button = False
     await asyncio.gather(janken, time)
 
 
-if st.button("ゲーム開始"):
-    start_event.set()
-    stop_event.clear()
-    frame_placeholder = st.empty()
+async def main() -> None:
+    if st.session_state.pushet_start_button:
+        start_event.set()
+        stop_event.clear()
+        frame_placeholder = st.empty()
+        st.session_state.pushet_start_button = False
+        await asyncio.gather(main_game(frame_placeholder))
+        st.rerun()
+    else:
+        if st.button("ゲーム開始"):
+            st.session_state.pushet_start_button = True
+            st.rerun()
+
+
+if __name__ == "__main__":
+    if "pushet_start_button" not in st.session_state:
+        st.session_state.pushet_start_button = False
     if "wrote_janken_result" not in st.session_state:
         st.session_state.wrote_janken_result = False
     if "set_finish_button" not in st.session_state:
